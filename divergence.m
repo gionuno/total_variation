d34a83990ef9b274f9fc1bdd49f04d2ab84ff611
Giovanni Nuño <gionuno@cimat.mx>
2019-07-03 13:30:11 -0500

    Update divergence.m
    oopsie

--- divergence.m
+++ divergence.m
@@ -1,15 +1,15 @@
 function divI = divergence(Ix,Iy)
     divI = zeros(size(Ix));
-    for i = 1:size(Ix,1)
-        for j = 1:size(Ix,2)
-            if 1 < i && i < size(Ix,1)
+    for i = 1:size(I,1)
+        for j = 1:size(I,2)
+            if 1 < i && i < size(I,1)
                 divI(i,j,:) = divI(i,j,:) - Ix(i-1,j,:) + Ix(i,j,:);
             elseif i == 1
                 divI(i,j,:) = divI(i,j,:) + Ix(i,j,:);
             else
                 divI(i,j,:) = divI(i,j,:) - Ix(i-1,j,:);
             end
-            if 1 < j && j < size(Ix,1)
+            if 1 < j && j < size(I,2)
                 divI(i,j,:) = divI(i,j,:) - Iy(i,j-1,:) + Iy(i,j,:);
             elseif j == 1
                 divI(i,j,:) = divI(i,j,:) + Iy(i,j,:);
